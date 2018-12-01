65cb9475c0c50f9c0b4060b966a81631461529a8
fujisawa <sfujisawa2016@gmail.com>
2018-12-01 12:06:16 +0900

    資料６のP4やってみよう３まで

--- Assets/TouchInput.cs
+++ Assets/TouchInput.cs
@@ -25,8 +25,31 @@ public class TouchInput : MonoBehaviour {
             // タッチの数分だけ、一点ずつ処理していく
             foreach(Touch touch in Input.touches)
             {
+                if (touch.phase == TouchPhase.Began)
+                {
+
+                }
+
+                switch(touch.phase)
+                {
+                    case TouchPhase.Began:
+                        text = "Began";
+                        break;
+                    case TouchPhase.Moved:
+                        text = "Moved";
+                        break;
+                    case TouchPhase.Stationary:
+                        text = "Stationary";
+                        break;
+                    case TouchPhase.Ended:
+                        text = "Ended";
+                        break;
+                    case TouchPhase.Canceled:
+                        text = "Canceled";
+                        break;
+                }
                 // 文字列の末尾に付け足す　　　　　　　　　　　　　　　　　　　　　改行コード
-                text += "touch" + touch.fingerId + ":(" + touch.position.x + "," + touch.position.y + ")" + "\n";
+                //text += "touch" + touch.fingerId + ":(" + touch.deltaTime + ")" + "\n";
             }
             // 文字列をテキストコンポーネントに反映させる
             textComponent.text = text;
